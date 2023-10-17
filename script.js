0b7cccbc8aca5a0148cae42f96392669df59639b
IrenaMartty <irenamartincevic3@gmail.com>
2023-10-17 23:39:12 +0200

    fixed css for the image container, and added js for the checkbox options

--- script.js
+++ script.js
@@ -62,13 +62,21 @@ $("#mode-button").addEventListener("click", () => {
 // <!-- TEXT PANEL -->
 // <!-- TOP TEXT --> <!-- BOTTOM TEXT -->
 
-$("#insert-top-text").addEventListener("input", (e) => {
+$("#insert-top-text").addEventListener("input", () => {
     $("#top-text").textContent = e.target.value
 })
+
+$("#checkbox-1").addEventListener("input", () => {
+    $("#top-text").classList.toggle("hidden")
+} )
 
 $("#insert-bottom-text").addEventListener("input", (e) => {
     $("#bottom-text").textContent = e.target.value
 })
+
+$("#checkbox-3").addEventListener("input", () => {
+    $("#bottom-text").classList.toggle("hidden")
+} )
 
 // <!-- FONT -->
 // <!-- FONT FAMILY -->
@@ -84,27 +92,71 @@ $("#font-size").addEventListener("input", (e) => {
     $("#bottom-text").style.fontSize = `${e.target.value}px`
 })
 
-$("#button-align-left").addEventListener("input", (e) => {
-    $("#top-text").style.textAlign = ("left")
-    $("#bottom-text").style.textAlign = ("left")
+$("#button-align-left").addEventListener("click", (e) => {
+    $("#top-text").style.textAlign = "left"
+    $("#bottom-text").style.textAlign = "left"
 })
 
-$("#button-align-center").addEventListener("input", (e) => {
+$("#button-align-center").addEventListener("click", (e) => {
     $("#top-text").style.textAlign = "center"
     $("#bottom-text").style.textAlign = "center"
 })
 
-$("#button-align-right").addEventListener("input", (e) => {
+$("#button-align-right").addEventListener("click", (e) => {
     $("#top-text").style.textAlign = 'right'
     $("#bottom-text").style.textAlign = 'right'
 })
+// <!-- FONT COLOR -->
+$("#pick-font-color").addEventListener("input", (e)=> {
+    $("#top-text").style.color = e.target.value
+    $("#bottom-text").style.color = e.target.value
+})
+// <!-- TEXT BACKGROUND COLOR -->
+$("#pick-text-background-color").addEventListener("input", (e)=> {
+    $("#top-text").style.backgroundColor = e.target.value
+    $("#bottom-text").style.backgroundColor = e.target.value
+})
 
+$("#checkbox").addEventListener("input", () => {
+    $("#top-text").classList.toggle("transparent")
+    $("#bottom-text").classList.toggle("transparent")
+} )
+
+// <!-- CONTOUR -->
+// $("#button-contour-none").addEventListener("click", (e) => {
+//     $("#top-text").style.addtextShadow = "none"
+//     $("#bottom-text").style.textShadow = "none"
+// })
+
+// $("#button-contour-light").addEventListener("click", (e) => {
+//     $("#top-text").style.textShadow = "white"
+//     $("#bottom-text").style.textShadow = "white"
+// })
+
+// $("#button-contour-dark").addEventListener("click", (e) => {
+//     $("#top-text").style.textShadow = "black"
+//     $("#bottom-text").style.textShadow = "black"
+// })
+
+// <!-- SPACING -->
+$("#spacing").addEventListener("input", (e) => {
+    $("#top-text").style.paddingTop = `${e.target.value}px`
+    $("#top-text").style.paddingBottom = `${e.target.value}px`
+    $("#bottom-text").style.paddingTop = `${e.target.value}px`
+    $("#bottom-text").style.paddingBottom = `${e.target.value}px`
+})    
+
+// <!-- LINE SPACING -->
+$("#select-spacing").addEventListener("input", (e) => {
+    $("#top-text").style.lineHeight = e.target.value
+    $("#bottom-text").style.lineHeight = e.target.value
+})    
 
 // <!-- IMAGE PANEL -->
 
 // <!-- INSERT URL -->
 const urlInput = document.getElementById('insert-img-url');
-const memeImg = document.getElementById('meme-img');
+const memeImg = document.getElementById('meme-template');
 
 urlInput.addEventListener('input', ()=> backgroundChange())
 
@@ -152,7 +204,3 @@ $("#button-default").addEventListener("click", () => {
     $("#invert-slider").value = "0"
     $(".meme-image").style.filter = "none"
 })
-
-
-
-
